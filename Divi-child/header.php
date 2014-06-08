e70be7e4c6c3225ca2f6855d66d87359372faa68
Mary Kimball <takeitawaymary@gmail.com>
2014-06-08 13:23:26 -0700

    Header updates

--- Divi-child/header.php
+++ Divi-child/header.php
@@ -130,7 +130,7 @@
 				<div id="et-top-navigation">
 					
 					<nav id="top-menu-nav">
-						<div class="mission"><em>Great events make life better.</em></div><br/><br/>
+						<div class="mission">Great events make life better.</div><br/><br/>
 					<?php
 						$menuClass = 'nav';
 						if ( 'on' == et_get_option( 'divi_disable_toptier' ) ) $menuClass .= ' et_disable_top_tier';
